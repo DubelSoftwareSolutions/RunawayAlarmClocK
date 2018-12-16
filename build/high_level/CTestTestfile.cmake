83fd9fdab036a6612b14d54c35115cf1ff2da9b3
Krzysztof Dabek <k.narg.dabek@gmail.com>
2018-12-16 11:27:18 +0100

    build removed

--- build/high_level/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kdabek/ROSWorkspace/src/high_level
-# Build directory: /home/kdabek/ROSWorkspace/build/high_level
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/low_level/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kdabek/ROSWorkspace/src/low_level
-# Build directory: /home/kdabek/ROSWorkspace/build/low_level
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
